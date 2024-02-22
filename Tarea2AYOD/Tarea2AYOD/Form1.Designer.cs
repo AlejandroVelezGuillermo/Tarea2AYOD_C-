ca9458bf8295413d02221bc97f5463122cf6845d
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2024-02-21 20:09:20 -0600

    Add files via upload

---
+++ Tarea2AYOD/Tarea2AYOD/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Tarea2AYOD
+{
+    partial class btnLeerAccesoDirecto
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnEscribir2 = new System.Windows.Forms.Button();
+            this.btnLeer2 = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtData1 = new System.Windows.Forms.TextBox();
+            this.btnCrearAccesoDirect = new System.Windows.Forms.Button();
+            this.btnLeerAccesoDirect = new System.Windows.Forms.Button();
+            this.btnModificarAccesoDirecto = new System.Windows.Forms.Button();
+            this.txtRutaArchivo = new System.Windows.Forms.TextBox();
+            this.txtRutaAccesoDirecto = new System.Windows.Forms.TextBox();
+            this.txtNuevoDestino = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.Location = new System.Drawing.Point(3, 23);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(75, 23);
+            this.btnEscribir.TabIndex = 0;
+            this.btnEscribir.Text = "ESCRIBIR";
+            this.btnEscribir.UseVisualStyleBackColor = true;
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(84, 23);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "LEER";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            // 
+            // btnEscribir2
+            // 
+            this.btnEscribir2.Location = new System.Drawing.Point(3, 81);
+            this.btnEscribir2.Name = "btnEscribir2";
+            this.btnEscribir2.Size = new System.Drawing.Size(75, 23);
+            this.btnEscribir2.TabIndex = 2;
+            this.btnEscribir2.Text = "ESCRIBIR";
+            this.btnEscribir2.UseVisualStyleBackColor = true;
+            // 
+            // btnLeer2
+            // 
+            this.btnLeer2.Location = new System.Drawing.Point(84, 81);
+            this.btnLeer2.Name = "btnLeer2";
+            this.btnLeer2.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer2.TabIndex = 3;
+            this.btnLeer2.Text = "LEER";
+            this.btnLeer2.UseVisualStyleBackColor = true;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(3, 53);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(156, 22);
+            this.txtData.TabIndex = 4;
+            // 
+            // txtData1
+            // 
+            this.txtData1.Location = new System.Drawing.Point(3, 110);
+            this.txtData1.Name = "txtData1";
+            this.txtData1.Size = new System.Drawing.Size(156, 22);
+            this.txtData1.TabIndex = 5;
+            // 
+            // btnCrearAccesoDirect
+            // 
+            this.btnCrearAccesoDirect.Location = new System.Drawing.Point(3, 184);
+            this.btnCrearAccesoDirect.Name = "btnCrearAccesoDirect";
+            this.btnCrearAccesoDirect.Size = new System.Drawing.Size(156, 23);
+            this.btnCrearAccesoDirect.TabIndex = 6;
+            this.btnCrearAccesoDirect.Text = "Crear Acceso Directo";
+            this.btnCrearAccesoDirect.UseVisualStyleBackColor = true;
+            this.btnCrearAccesoDirect.Click += new System.EventHandler(this.btnCrearAccesoDirect_Click);
+            // 
+            // btnLeerAccesoDirect
+            // 
+            this.btnLeerAccesoDirect.Location = new System.Drawing.Point(166, 183);
+            this.btnLeerAccesoDirect.Name = "btnLeerAccesoDirect";
+            this.btnLeerAccesoDirect.Size = new System.Drawing.Size(166, 23);
+            this.btnLeerAccesoDirect.TabIndex = 7;
+            this.btnLeerAccesoDirect.Text = "Leer Acceso Directo";
+            this.btnLeerAccesoDirect.UseVisualStyleBackColor = true;
+            this.btnLeerAccesoDirect.Click += new System.EventHandler(this.btnLeerAccesoDirect_Click);
+            // 
+            // btnModificarAccesoDirecto
+            // 
+            this.btnModificarAccesoDirecto.Location = new System.Drawing.Point(339, 183);
+            this.btnModificarAccesoDirecto.Name = "btnModificarAccesoDirecto";
+            this.btnModificarAccesoDirecto.Size = new System.Drawing.Size(208, 23);
+            this.btnModificarAccesoDirecto.TabIndex = 8;
+            this.btnModificarAccesoDirecto.Text = "Modificar Acceso Directo";
+            this.btnModificarAccesoDirecto.UseVisualStyleBackColor = true;
+            this.btnModificarAccesoDirecto.Click += new System.EventHandler(this.btnModificarAccesoDirecto_Click);
+            // 
+            // txtRutaArchivo
+            // 
+            this.txtRutaArchivo.Location = new System.Drawing.Point(22, 234);
+            this.txtRutaArchivo.Name = "txtRutaArchivo";
+            this.txtRutaArchivo.Size = new System.Drawing.Size(100, 22);
+            this.txtRutaArchivo.TabIndex = 9;
+            // 
+            // txtRutaAccesoDirecto
+            // 
+            this.txtRutaAccesoDirecto.Location = new System.Drawing.Point(13, 263);
+            this.txtRutaAccesoDirecto.Name = "txtRutaAccesoDirecto";
+            this.txtRutaAccesoDirecto.Size = new System.Drawing.Size(100, 22);
+            this.txtRutaAccesoDirecto.TabIndex = 10;
+            // 
+            // txtNuevoDestino
+            // 
+            this.txtNuevoDestino.Location = new System.Drawing.Point(47, 313);
+            this.txtNuevoDestino.Name = "txtNuevoDestino";
+            this.txtNuevoDestino.Size = new System.Drawing.Size(100, 22);
+            this.txtNuevoDestino.TabIndex = 11;
+            // 
+            // btnLeerAccesoDirecto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNuevoDestino);
+            this.Controls.Add(this.txtRutaAccesoDirecto);
+            this.Controls.Add(this.txtRutaArchivo);
+            this.Controls.Add(this.btnModificarAccesoDirecto);
+            this.Controls.Add(this.btnLeerAccesoDirect);
+            this.Controls.Add(this.btnCrearAccesoDirect);
+            this.Controls.Add(this.txtData1);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnLeer2);
+            this.Controls.Add(this.btnEscribir2);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEscribir);
+            this.Name = "btnLeerAccesoDirecto";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnEscribir2;
+        private System.Windows.Forms.Button btnLeer2;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtData1;
+        private System.Windows.Forms.Button btnCrearAccesoDirect;
+        private System.Windows.Forms.Button btnLeerAccesoDirect;
+        private System.Windows.Forms.Button btnModificarAccesoDirecto;
+        private System.Windows.Forms.TextBox txtRutaArchivo;
+        private System.Windows.Forms.TextBox txtRutaAccesoDirecto;
+        private System.Windows.Forms.TextBox txtNuevoDestino;
+    }
+}
+
---
+++ Tarea2AYOD/Tarea2AYOD/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace Tarea2AYOD
+{
+    public partial class btnLeerAccesoDirecto : Form
+    {
+        string filePath = "datos.txt"; // Ruta del archivo
+
+        string dataFilePath = "datos.dat"; // Ruta del archivo de datos
+        string indexFilePath = "index.dat"; // Ruta del archivo de índice
+        public btnLeerAccesoDirecto()
+        {
+            InitializeComponent();
+        }
+        //Implementar las operaciones para archivos secuenciales.
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            string data = txtData.Text;
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine(data);
+                    }
+
+                    MessageBox.Show("Datos escritos en el archivo correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al escribir en el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese datos antes de escribir en el archivo.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string content = reader.ReadToEnd();
+                    MessageBox.Show("Contenido del archivo:\n" + content, "Lectura exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        //Implementar las operaciones para archivos secuenciales indexados.
+        private void btnEscribir2_Click(object sender, EventArgs e)
+        {
+            string data = txtData1.Text;
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                try
+                {
+                    long position;
+                    using (FileStream dataFileStream = new FileStream(dataFilePath, FileMode.Append, FileAccess.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(dataFileStream))
+                        {
+                            position = dataFileStream.Position;
+                            writer.WriteLine(data);
+                        }
+                    }
+
+                    using (FileStream indexFileStream = new FileStream(indexFilePath, FileMode.Append, FileAccess.Write))
+                    {
+                        using (BinaryWriter indexWriter = new BinaryWriter(indexFileStream))
+                        {
+                            indexWriter.Write(position);
+                        }
+                    }
+
+                    MessageBox.Show("Datos escritos en el archivo correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al escribir en el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese datos antes de escribir en el archivo.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnLeer2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> dataRecords = new List<string>();
+
+                using (FileStream indexFileStream = new FileStream(indexFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader indexReader = new BinaryReader(indexFileStream))
+                    {
+                        while (indexFileStream.Position < indexFileStream.Length)
+                        {
+                            long position = indexReader.ReadInt64();
+
+                            using (FileStream dataFileStream = new FileStream(dataFilePath, FileMode.Open, FileAccess.Read))
+                            {
+                                dataFileStream.Seek(position, SeekOrigin.Begin);
+
+                                using (StreamReader dataReader = new StreamReader(dataFileStream))
+                                {
+                                    string data = dataReader.ReadLine();
+                                    dataRecords.Add(data);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                string content = string.Join(Environment.NewLine, dataRecords);
+                MessageBox.Show("Contenido del archivo:\n" + content, "Lectura exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+
+
+
+        //Implementar las operaciones para archivos de acceso directo.
+        private void btnCrearAccesoDirect_Click(object sender, EventArgs e)
+        {
+            // Obtener la ruta del archivo al que se quiere crear el acceso directo
+            string archivoObjetivo = txtRutaArchivo.Text;
+
+            // Obtener la ruta y nombre para el nuevo acceso directo
+            string rutaAccesoDirecto = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "MiAccesoDirecto.lnk");
+
+            // Crear el objeto de acceso directo
+            var accesoDirecto = new WshShell().CreateShortcut(rutaAccesoDirecto) as IWshShortcut;
+
+            // Configurar las propiedades del acceso directo
+            if (accesoDirecto != null)
+            {
+                accesoDirecto.TargetPath = archivoObjetivo;
+                accesoDirecto.Save();
+                MessageBox.Show("Acceso directo creado con éxito.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnLeerAccesoDirect_Click(object sender, EventArgs e)
+        {
+            // Obtener la ruta del acceso directo a leer
+            string rutaAccesoDirecto = txtRutaAccesoDirecto.Text;
+
+            try
+            {
+                // Verificar si el archivo de acceso directo existe
+                if (System.IO.File.Exists(rutaAccesoDirecto))
+                {
+                    // Leer el acceso directo
+                    var accesoDirecto = new WshShell().CreateShortcut(rutaAccesoDirecto) as IWshShortcut;
+
+                    // Mostrar la información del acceso directo
+                    if (accesoDirecto != null)
+                    {
+                        MessageBox.Show($"Información del acceso directo:\n\n" +
+                                        $"Destino: {accesoDirecto.TargetPath}\n" +
+                                        $"Icono: {accesoDirecto.IconLocation}\n" +
+                                        $"Trabaja en: {accesoDirecto.WorkingDirectory}",
+                                        "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El archivo de acceso directo no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al intentar leer el acceso directo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnModificarAccesoDirecto_Click(object sender, EventArgs e)
+        {
+            // Obtener la ruta del acceso directo a modificar
+            string rutaAccesoDirecto = txtRutaAccesoDirecto.Text;
+
+            // Verificar si el archivo de acceso directo existe
+            if (System.IO.File.Exists(rutaAccesoDirecto))
+            {
+                // Leer el acceso directo
+                var accesoDirecto = new WshShell().CreateShortcut(rutaAccesoDirecto) as IWshShortcut;
+
+                // Modificar el acceso directo
+                if (accesoDirecto != null)
+                {
+                    accesoDirecto.TargetPath = txtNuevoDestino.Text;
+                    accesoDirecto.Save();
+                    MessageBox.Show("Acceso directo modificado con éxito.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo de acceso directo no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
